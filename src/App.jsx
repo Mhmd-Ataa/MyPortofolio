b2d2e79392d0957475a191d7149db44edabeaad1
Adham Magdy <magdyadham229@gmail.com>
2026-03-30 00:22:51 +0200

    remove word not used2

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,28 @@
-
-import { useEffect, useState } from 'react';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import LayOut from './components/LayOut';
-import Contacts from './components/Pages/contacts/Contacts';
-import Home from './components/Pages/Home/Home';
-import Main from './components/Pages/main/Main';
-import Resume from './components/Pages/Resume/Resume';
-
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import LayOut from "./components/LayOut";
+import Contacts from "./components/Pages/contacts/Contacts";
+import Home from "./components/Pages/Home/Home";
+import Main from "./components/Pages/main/Main";
+import Resume from "./components/Pages/Resume/Resume";
 
 function App() {
-
- 
-
-
   let routers = createBrowserRouter([
     {
-      path: "", element: <LayOut />, children: [
+      path: "",
+      element: <LayOut />,
+      children: [
         { index: true, element: <Home /> },
         { path: "contact", element: <Contacts /> },
-        { path: "Projects", element: <Main/> },
-        { path: "Resume", element: <Resume/> }
-      ]
-    }
-
-  ])
-  return <>
-
-
-<RouterProvider router = {routers}></RouterProvider>
-
-   
-  </>
-
-
+        { path: "Projects", element: <Main /> },
+        { path: "Resume", element: <Resume /> },
+      ],
+    },
+  ]);
+  return (
+    <>
+      <RouterProvider router={routers}></RouterProvider>
+    </>
+  );
 }
 
 export default App;
